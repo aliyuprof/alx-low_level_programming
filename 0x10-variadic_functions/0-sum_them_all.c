c6c22eadebe3424446e9ee31b0fcb21a74c6d913
Aliyu <aliyuddeen95@gmail.com>
2023-08-18 15:32:17 -0700

    First variadity project commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/*
+ * Author: Ali Usman Mohammed
+ * Task - Variadic function assigment of the ALX program.
+ * Variadic function is a function that return the sum of all its parameters
+ * Always return (0)
+*/
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int counter, increment = 0;
+	va_list numList;
+
+	va_start(numList, n);
+
+	for(counter = 0; counter < n;  counter++);
+
+	increment+= va_arg(numList, int);
+	va_end (numList);
+	return (increment);
+		
+}
