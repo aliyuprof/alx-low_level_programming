c49aa9c2667b3ff060366f9efb2fd865eb68678d
aliyuprof <aliyuddeen95@gmail.com>
2024-03-05 05:24:45 -0800

    FIRST Question Updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,14 +7,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 int i;
-unsigned int digit = 0;
+unsigned int number = 0;
 if (!b)
 return (0);
 for (i = 0; b[i]; i++)
 {
 if (b[i] != '0' && b[i] != '1')
 return (0);
-digit = 2 * digit + (b[i] - '0');
+number = 2 * number + (b[i] - '0');
 }
-return (digit);
+return (number);
 }
